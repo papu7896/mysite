4b9aaed40b4260a0e7b45e82f368406e276344a1
papu7896 <124523674+papu7896@users.noreply.github.com>
2023-03-05 16:32:32 +0530

    Revert "responsive home"

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,86 +1,38 @@
-import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
+import React from 'react'
+import {NavLink} from "react-router-dom";
+
 
 const navbar = () => {
-  const [toggle, setToggle] = useState(false);
   const activeStyle = {
     textDecoration: "underline",
-    color: "yellow",
+    color: "yellow"
+    
   };
-  const normal = {
+  const normal={
     textDecoration: "none",
-    color: "white",
-  };
+  color: "white",
+  }
   return (
-    <div className="nav" style={{ backgroundColor: "black", padding: "10px" }}>
-      <div style={{ display: "flex", justifyContent: "flex-end" }}>
-        <button onClick={() => setToggle(!toggle)}>menu</button>
-      </div>
-      {toggle && (
-        <div>
-          <div className="navLink">
-            <NavLink
-              to="/home"
-              style={({ isActive }) => (isActive ? activeStyle : normal)}
-            >
-              HOME
-            </NavLink>
-          </div>
-          <div className="navLink">
-            <NavLink
-              to="/skills"
-              style={({ isActive }) => (isActive ? activeStyle : normal)}
-            >
-              SKILLS
-            </NavLink>
-          </div>
-          <div className="navLink">
-            <NavLink
-              to="/experience"
-              style={({ isActive }) => (isActive ? activeStyle : normal)}
-            >
-              EXPERIENCE
-            </NavLink>
-          </div>
-          <div className="navLink">
-            <NavLink
-              to="/contact "
-              style={({ isActive }) => (isActive ? activeStyle : normal)}
-            >
-              CONTACT{" "}
-            </NavLink>
-          </div>
+    <div className="nav" style={{backgroundColor:'black',height:"1.5cm"}}>
+        <div className="link">
+    <NavLink to="/home" style={({ isActive }) =>
+              isActive ? activeStyle :  normal
+            }>HOME</NavLink>
+  
+  <NavLink to="/skills" style={({ isActive }) =>
+              isActive ? activeStyle :  normal
+            }>SKILLS</NavLink>
+  <NavLink to="/experience" style={({ isActive }) =>
+              isActive ? activeStyle :  normal
+            }>EXPERIENCE</NavLink>
+  <NavLink to="/contact " style={({ isActive }) =>
+              isActive ?activeStyle : normal
+            }>CONTACT </NavLink>
         </div>
-      )}
-      <div className="link">
-        <NavLink
-          to="/home"
-          style={({ isActive }) => (isActive ? activeStyle : normal)}
-        >
-          HOME
-        </NavLink>
-
-        <NavLink
-          to="/skills"
-          style={({ isActive }) => (isActive ? activeStyle : normal)}
-        >
-          SKILLS
-        </NavLink>
-        <NavLink
-          to="/experience"
-          style={({ isActive }) => (isActive ? activeStyle : normal)}
-        >
-          EXPERIENCE
-        </NavLink>
-        <NavLink
-          to="/contact "
-          style={({ isActive }) => (isActive ? activeStyle : normal)}
-        >
-          CONTACT{" "}
-        </NavLink>
-      </div>
     </div>
-  );
-};
+    
+  
+  )
+}
 
 export default navbar;
